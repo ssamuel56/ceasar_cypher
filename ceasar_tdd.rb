bcee67fc195797dfd18dcd056397964aeb4ff544
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-10-31 15:19:50 -0400

    first commit

---
+++ ceasar_tdd.rb
@@ -0,0 +1,11 @@
+require_relative 'ceasar.rb'
+require 'minitest/autorun'
+
+class Test_for_cipher < Minitest::Test
+  def test_1for1
+    assert_equal(1, 1)
+  end
+  def test_for_combinations
+    assert_equal([], ceazar("b", encrypt))
+  end
+end
