70c0d64b939317594250976c07275ba2d456b260
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-10-31 17:16:15 -0400

    Testing my hard string with the regular function

--- ceasar_tdd.rb
+++ ceasar_tdd.rb
@@ -20,11 +20,14 @@ class Test_for_cipher < Minitest::Test
   def test_for_a_longer_message
     assert_equal("ymj jsjrd fuwtfhmjx. bj fwj qtb ts xzuuqnjx; tzw rjs fwj ltnsl yt xyfwaj tw kwjjej!", ceasar("The enemy aproaches. We are low on supplies; our men are going to starve or freeze!"))
   end
-  def test_for_a_comma
+  def test_reverse_for_a_comma
     assert_equal("send help, please", ceasar_reverse("xjsi mjqu, uqjfxj"))
   end
   def test_for_a_hard_reverse
     assert_equal("<xjcb> <ishyfj></ishyfj> </xjcb> %%%", ceasar_reverse("<cohg> <nxmdko></nxmdko> </cohg> %%%"))
+  end
+  def test_for_a_hard_string
+    assert_equal(" ", ceasar_reverse("<cohg> <nxmdko></nxmdko> </cohg> %%%"))
   end
   def test_ceaser_with_no_string_as_argument
     assert_nil(nil, ceasar(123))
