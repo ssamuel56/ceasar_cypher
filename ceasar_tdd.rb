d6e97e25daeacf42ef1b1e623fc62ecf62fc70fa
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-10-31 17:07:27 -0400

    Testing more long strings

--- ceasar_tdd.rb
+++ ceasar_tdd.rb
@@ -17,11 +17,14 @@ class Test_for_cipher < Minitest::Test
   def test_for_a_comma
     assert_equal("xjsi mjqu, uqjfxj", ceasar("send help, please"))
   end
+  def test_for_a_longer_message
+    assert_equal(" ", ceasar("The enemy aproaches. We are low on supplies; our men are going to starve or freeze!"))
+  end
   def test_for_a_comma
     assert_equal("send help, please", ceasar_reverse("xjsi mjqu, uqjfxj"))
   end
   def test_for_a_hard_reverse
-    assert_equal("<cohg> <nxmdko></nxmdko> </cohg> %%%", ceasar_reverse("<html> <script></script> </html> %%%"))
+    assert_equal("<xjcb> <ishyfj></ishyfj> </xjcb> %%%", ceasar_reverse("<cohg> <nxmdko></nxmdko> </cohg> %%%"))
   end
   def test_ceaser_with_no_string_as_argument
     assert_nil(nil, ceasar(123))
