6b0b71d8e8e208769d46acd596d12265c581454d
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-11-06 00:35:20 -0500

    Made the time work for 10 and a fail test for any day

--- ceasar.rb
+++ ceasar.rb
@@ -1,6 +1,5 @@
 def ceasar(string, rotate_by = Time.now.day)
-  arr = ("a".."z").to_a; arr2 = Array.new; temp_arr = Array.new; arr.each.with_index {|x, i| arr2[i] = arr[i - (16 + rotate_by)]}
-  p "This is arr2 #{arr2}"
+  arr = ("a".."z").to_a; arr2 = Array.new; temp_arr = Array.new; arr.each.with_index {|x, i| arr2 = arr.rotate(rotate_by)}
   if string.class == String
     string.downcase.split('').each do |char|
       if char.count("a-z") < 1
@@ -17,8 +16,8 @@ def ceasar(string, rotate_by = Time.now.day)
   end
 end
 
-def ceasar_reverse(string, rotate_by = 5)
-  arr = ("a".."z").to_a; arr2 = Array.new; temp_arr = Array.new; arr.each.with_index {|x, i| arr2[i] = arr[i - (16 + rotate_by)]}
+def ceasar_reverse(string, rotate_by = Time.now.day)
+  arr = ("a".."z").to_a; arr2 = Array.new; temp_arr = Array.new; arr.each.with_index {|x, i| arr2 = arr.rotate(rotate_by)}
   if string.class == String
     string.downcase.split('').each do |char|
       if char.count("a-z") < 1
--- ceasar_tdd.rb
+++ ceasar_tdd.rb
@@ -6,40 +6,40 @@ class Test_for_cipher < Minitest::Test
     assert_equal(1, 1)
   end
   def test_for_combinations
-    assert_equal("g", ceasar("b"))
+    assert_equal("g", ceasar("b", 5))
   end
   def test_for_help
-    assert_equal("mjqu", ceasar("help"))
+    assert_equal("mjqu", ceasar("help", 5))
   end
   def test_for_send_help
-    assert_equal("xjsi mjqu", ceasar("send help"))
+    assert_equal("xjsi mjqu", ceasar("send help", 5))
   end
   def test_for_a_comma
-    assert_equal("xjsi mjqu, uqjfxj", ceasar("send help, please"))
+    assert_equal("xjsi mjqu, uqjfxj", ceasar("send help, please", 5))
   end
   def test_for_a_longer_message
-    assert_equal("ymj jsjrd fuwtfhmjx. bj fwj qtb ts xzuuqnjx; tzw rjs fwj ltnsl yt xyfwaj tw kwjjej!", ceasar("The enemy aproaches. We are low on supplies; our men are going to starve or freeze!"))
+    assert_equal("ymj jsjrd fuwtfhmjx. bj fwj qtb ts xzuuqnjx; tzw rjs fwj ltnsl yt xyfwaj tw kwjjej!", ceasar("The enemy aproaches. We are low on supplies; our men are going to starve or freeze!", 5))
   end
   def test_reverse_for_a_comma
-    assert_equal("send help, please", ceasar_reverse("xjsi mjqu, uqjfxj"))
+    assert_equal("send help, please", ceasar_reverse("xjsi mjqu, uqjfxj", 5))
   end
   def test_for_another_simple_reverse
-    assert_equal("send help, please", ceasar_reverse("xjsi mjqu, uqjfxj"))
+    assert_equal("send help, please", ceasar_reverse("xjsi mjqu, uqjfxj", 5))
   end
   def test_for_a_hard_reverse
-    assert_equal("<sexw> <dnctae></dnctae> </sexw> %%%", ceasar_reverse("<xjcb> <ishyfj></ishyfj> </xjcb> %%%"))
+    assert_equal("<sexw> <dnctae></dnctae> </sexw> %%%", ceasar_reverse("<xjcb> <ishyfj></ishyfj> </xjcb> %%%", 5))
   end
   def test_for_reverse_same
-    assert_equal("a", ceasar_reverse("F"))
+    assert_equal("a", ceasar_reverse("F", 5))
   end
   def test_for_a_hard_string
-    assert_equal("<xjcb> <ishyfj></ishyfj> </xjcb> %%%", ceasar_reverse("<cohg> <nxmdko></nxmdko> </cohg> %%%"))
+    assert_equal("<xjcb> <ishyfj></ishyfj> </xjcb> %%%", ceasar_reverse("<cohg> <nxmdko></nxmdko> </cohg> %%%", 5))
   end
   def test_for_numbers
-    assert_equal("123", ceasar("123"))
+    assert_equal("123", ceasar("123", 5))
   end
   def test_for_numbers
-    assert_equal("123", ceasar_reverse("123"))
+    assert_equal("123", ceasar_reverse("123", 5))
   end
   def test_ceaser_with_no_string_as_argument
     assert_nil(nil, ceasar(123))
@@ -51,10 +51,14 @@ class Test_for_cipher < Minitest::Test
     day_of_month = 5
     assert_equal("yjxy ktw 5", ceasar("Test for 5", 5))
   end
-  def test_with_time_for_day_5
-    assert_equal("yjxy ktw 5", ceasar("Test for 5", 5))
-  end
   def test_with_time_for_day_10
-    assert_equal("yjxy ktw 10", ceasar("Test for 10", 10))
+    assert_equal("docd pyb 10", ceasar("Test for 10", 10))
+  end
+  def test_with_time_for_day_6
+    assert_equal("", ceasar("String of letters", 6))
+  end
+  def test_for_any_day
+    day = Time.now.day
+    assert_equal("", ceasar("String of letters", day))
   end
 end
